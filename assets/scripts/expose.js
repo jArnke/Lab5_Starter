fab1b878d8320b06358f632dccc0285fda989a39
jArnke <jyenney@ucsd.edu>
2022-10-30 14:54:30 -0700

    added skeleton code

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,32 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  var hornSelect = document.getElementById('#horn-select');
+  hornSelect.addEventListener('input', updateHorn);
+  var volumeSlider = document.getElementById('#volume');
+  volumeSlider.addEventListener('input', updateVolume);
+ // var playButton = document.getElement
+ // var image = document.getElement 
+
+}
+
+function updateHorn(e) {
+  switch (e.target.value){
+    case 'air-horn':
+      
+    case 'car-horn':
+    case 'party-horn':
+  }
+}
+
+function updateVolume(e) {
+
+}
+
+function playSound(e) {
+
+}
+
+function displayConfetti(){
+
 }
