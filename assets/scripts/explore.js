93e55efd1b0cb383cf3cff972c1cebdf107bc750
jArnke <jyenney@ucsd.edu>
2022-10-30 16:04:00 -0700

    explore done

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,54 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+var playButton;
+var textArea;
+var image;
+var voiceSelect;
+
 function init() {
-  // TODO
+  image = document.querySelector('img');
+  playButton = document.querySelector('button');
+  textArea = document.getElementById('text-to-speak');
+  playButton.addEventListener('click', speak);
+  voiceSelect = document.getElementById('voice-select');
+
+  //Fill voice list
+  const voices = speechSynthesis.getVoices();
+  for (let i =0; i < voices.length; i++){
+    let voice = voices[i];
+    const option = document.createElement('option');
+    option.textContent = `${voice.name} (${voice.lang})`;
+
+    option.setAttribute('data-lang', voice.lang);
+    option.setAttribute('data-name', voice.name);
+    voiceSelect.appendChild(option)
+
+  }
+
+  setInterval(checkIfSpeaking, 1);
+}
+
+function speak() {
+  let utterance = new SpeechSynthesisUtterance(textArea.value);
+  let voices = speechSynthesis.getVoices();
+  let voice = voiceSelect.selectedOptions[0].getAttribute('data-name');
+  for (let i = 0; i < voices.length ; i++) {
+    if (voices[i].name === voice) {
+      utterance.voice = voices[i];
+    }
+  }
+
+  speechSynthesis.speak(utterance);
+
+}
+
+function checkIfSpeaking() {
+  console.log(speechSynthesis.speaking);
+  if (speechSynthesis.speaking){
+    image.src = 'assets/images/smiling-open.png';
+  }
+  else{
+    image.src = 'assets/images/smiling.png';
+  }
 }
